81af5674b6d024eae753fd37c8c2b735352fad6a
Warren Parad <wparad@gmail.com>
2013-12-17 21:09:30 -0500

    update manifest for the desktop

--- desktop.pp
+++ desktop.pp
@@ -6,12 +6,17 @@ node default{
 	package{'geany': }
 	package{'mono-complete':}
 	package{'scrot':}
+	package{'vlc':}
+	package{'chromium-browser':}
 
 	#for drivers for steam video
 	package{'libelf-dev':}
 	package{'libc6-dev-i386':}
 	package{'git':}
 	package{'build-essential':}
+	package{'xfce4-mixer':}
+	package{'pavucontrol':}
+	package{'autokey-gtk':}
 #	file{"xfce hide_unhide app bar":
 #		ensure => present,
 #		content =>
@@ -26,7 +31,17 @@ node default{
                 group  => "root",
                 mode   => 777
         }
-
-
-
+#	
+#	vlc setup
+#	file{'/etc/vlc/lua/http/.hosts'
+#		ensure => present,
+#		owner  => "root"
+#		group  => "root",
+#               mode   => 755,
+#		content => 'add in the updated host value'
+#	}
+#
+#
+#
+#
 }

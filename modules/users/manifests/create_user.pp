f7bbb7cf26d350105027db4b20638fda484ddb24
Warren Parad <wparad@gmail.com>
2014-08-23 13:36:47 -0400

    Do not require ssh keys for service users.

--- modules/users/manifests/create_user.pp
+++ modules/users/manifests/create_user.pp
@@ -2,52 +2,55 @@ define users::create_user($ingroups, $user_shell = '/bin/bash', $id, $email = 'n
 {
         $user_home_dir = "/home/$name"
 		group{$name:
-               ensure => present,
-               gid => $id
+			ensure => present,
+			gid => $id
         }
 
-	-> user{$name:
-                ensure => present,
-		uid => $id,
-                gid => $name,
-                groups => $ingroups,
-                membership => minimum,
-                shell => $user_shell,
-                home => $user_home_dir,
-                require => Group[$name]
+		-> user{$name:
+            ensure => present,
+			uid => $id,
+			gid => $name,
+			groups => $ingroups,
+			membership => minimum,
+			shell => $user_shell,
+			home => $user_home_dir,
+			require => Group[$name]
         }
 
         $user_ssh_dir = "${user_home_dir}/.ssh"
         exec {"$name homedir":
-                command => "/bin/cp -R /etc/skel $user_home_dir; /bin/chown -R ${name}:${name} ${user_home_dir}",
-                creates => $user_home_dir,
-                require => User[$name]
+			command => "/bin/cp -R /etc/skel $user_home_dir; /bin/chown -R ${name}:${name} ${user_home_dir}",
+			creates => $user_home_dir,
+			require => User[$name]
         }
-        -> file{$user_ssh_dir:
-                ensure => directory,
-                owner => $name,
-                group => $name,
-                mode => 0700
-        }
-        -> file{"${user_ssh_dir}/authorized_keys":
-                ensure => file,
-                owner => $name,
-                group => $name,
-                mode => 0700,
-                source => "puppet:///modules/users/$name/.ssh/authorized_keys"
-        }
-        -> file{"${user_home_dir}/.bashrc":
-                ensure => file,
-                owner => $name,
-                group => $name,
-                mode => 0644,
-                source => "puppet:///modules/users/$name/.bashrc"
-        }
-        -> file{"${user_home_dir}/.forward":
-                ensure => present,
-                owner  => warren,
-                group  => warren,
-                mode   => '0755',
-                content => $email
-        }
+        if $user_shell != '/bin/false'{
+			Exec["$name homedir"]
+			-> file{$user_ssh_dir:
+				ensure => directory,
+				owner => $name,
+				group => $name,
+				mode => 0700
+			}
+			-> file{"${user_ssh_dir}/authorized_keys":
+				ensure => file,
+				owner => $name,
+				group => $name,
+				mode => 0700,
+				source => "puppet:///modules/users/$name/.ssh/authorized_keys"
+			}
+			-> file{"${user_home_dir}/.bashrc":
+				ensure => file,
+				owner => $name,
+				group => $name,
+				mode => 0644,
+				source => "puppet:///modules/users/$name/.bashrc"
+			}
+			-> file{"${user_home_dir}/.forward":
+				ensure => present,
+				owner  => warren,
+				group  => warren,
+				mode   => '0755',
+				content => $email
+			}
+		}
 }

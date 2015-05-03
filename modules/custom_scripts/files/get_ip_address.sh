f5d2cc216fc98fe2beb011f18ffdbc2893758ca0
Warren Parad <wparad@gmail.com>
2015-05-03 17:47:47 -0400

    Remove debug code.

--- modules/custom_scripts/files/get_ip_address.sh
+++ modules/custom_scripts/files/get_ip_address.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 IP=`wget -O - -q icanhazip.com`
-#echo $IP
 FILE="/tmp/ipAddress"
 echo "" >> $FILE
-#exit 0
 
-if [ "~$IP~" == "~$(cat $FILE | cut -d'~' -f2)~" ]
-	then
-#echo "~$IP~"
-#echo "~$(cat $FILE | cut -d'~' -f2)~"
-#echo "MATCH YES"
-	exit 0
-fi
+[ "~$IP~" == "~$(cat $FILE | cut -d'~' -f2)~" ] && exit 0
 
 rm -f $FILE
-#echo "NO MATCH"
+
 SUBJECT="`hostname` Server IP Address"
 echo "~$IP~" > $FILE
-echo "$IP `ifconfig`" | mail -aFrom:wparad@gmail.com -s "$SUBJECT" wparad@gmail.com
+echo "$IP `/sbin/ifconfig`" | mail -aFrom:wparad@gmail.com -s "$SUBJECT" wparad@gmail.com
+
+exit 0

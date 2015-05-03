5a8b56b0e5b526fa5b990e547df28a9509d83310
Warren Parad <wparad@gmail.com>
2015-05-03 16:47:28 -0400

    Fix exist syntax.

--- modules/library/lib/puppet/parser/functions/get_passwords.rb
+++ modules/library/lib/puppet/parser/functions/get_passwords.rb
@@ -3,7 +3,7 @@ require 'json'
 module Puppet::Parser::Functions
   newfunction(:get_passwords, :type => :rvalue) do |args|
 	filename = '/passwords.json'
-	raise '/passwords.json file does exist' if !File.exists(filename)
+	raise '/passwords.json file does exist' if !File.exist?(filename)
 	JSON::parse(File.read(filename))
   end
 end

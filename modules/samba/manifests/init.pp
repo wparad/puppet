09cbeace85cdc33940bba757f61138cadae519c4
Warren Parad <wparad@gmail.com>
2015-07-27 20:05:58 -0400

    Fix puppet manifest for samba.

--- modules/samba/manifests/init.pp
+++ modules/samba/manifests/init.pp
@@ -2,14 +2,11 @@ class samba()
 {
         package{'samba':}
         -> package{'cifs-utils':}
-	-> exec{'([ -f /etc/samba/smb.conf ] && ! [ -f /etc/samba/smb.conf.bak ]) && mv /etc/samba/smb.conf /etc/samba/smb.conf smb.conf.bak':
-		path => $::path
-	}
-	-> file{'/etc/samaba/smb.conf'
-	        ensure => file,
+	-> file{'/etc/samaba/smb.conf':
+	        ensure => 'file',
                 owner => root,
                 group => root,
-                mode => 0644,
+                mode => '0644',
                 source => "puppet:///modules/samba/smb.conf"
         }
 

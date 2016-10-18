788debca8fe2b9a3a5c45644b1f8eb1e32b00292
Warren Parad <wparad@cimpress.com>
2016-10-18 13:18:11 -0400

    Add vscode settings.

--- modules/users/manifests/create_user.pp
+++ modules/users/manifests/create_user.pp
@@ -1,10 +1,10 @@
 define users::create_user($ingroups, $user_shell = '/bin/bash', $id, $email = 'no-reply@gmail.com')
 {
-        $user_home_dir = "/home/$name"
+	$user_home_dir = "/home/$name"
 	group{$name:
 		ensure => present,
 		gid => $id
-        }
+	}
 
 	-> user{$name:
 		ensure => present,
@@ -15,49 +15,62 @@ define users::create_user($ingroups, $user_shell = '/bin/bash', $id, $email = 'n
 		shell => $user_shell,
 		home => $user_home_dir,
 		require => Group[$name]
-        }
+	}
 
-        $user_ssh_dir = "${user_home_dir}/.ssh"
-        exec {"$name homedir":
-			command => "/bin/cp -R /etc/skel $user_home_dir; /bin/chown -R ${name}:${name} ${user_home_dir}",
-			creates => $user_home_dir,
-			require => User[$name]
-        }
-        if $user_shell != '/bin/false' {
-			Exec["$name homedir"]
-			-> file{$user_ssh_dir:
-				ensure => directory,
-				owner => $name,
-				group => $name,
-				mode => '0700'
-			}
-			-> file{"${user_ssh_dir}/authorized_keys":
-				ensure => file,
-				owner => $name,
-				group => $name,
-				mode => '0700',
-				source => "puppet:///modules/users/$name/.ssh/authorized_keys"
-			}
-			-> file{"${user_home_dir}/.bashrc":
-				ensure => file,
-				owner => $name,
-				group => $name,
-				mode => '0644',
-				source => "puppet:///modules/users/$name/.bashrc"
-			}
-			-> file{"${user_home_dir}/.gitconfig":
-				ensure => file,
-				owner => $name,
-				group => $name,
-				mode => '0640',
-				source => "puppet:///modules/users/$name/.gitconfig"
-			}
-			-> file{"${user_home_dir}/.forward":
-				ensure => present,
-				owner  => warren,
-				group  => warren,
-				mode   => '0755',
-				content => $email
-			}
+	$user_ssh_dir = "${user_home_dir}/.ssh"
+	exec {"$name homedir":
+		command => "/bin/cp -R /etc/skel $user_home_dir; /bin/chown -R ${name}:${name} ${user_home_dir}",
+		creates => $user_home_dir,
+		require => User[$name]
+	}
+	if $user_shell != '/bin/false' {
+		Exec["$name homedir"]
+		-> file{$user_ssh_dir:
+			ensure => directory,
+			owner => $name,
+			group => $name,
+			mode => '0700'
 		}
+		-> file{"${user_ssh_dir}/authorized_keys":
+			ensure => file,
+			owner => $name,
+			group => $name,
+			mode => '0700',
+			source => "puppet:///modules/users/$name/.ssh/authorized_keys"
+		}
+		-> file{"${user_home_dir}/.bashrc":
+			ensure => file,
+			owner => $name,
+			group => $name,
+			mode => '0644',
+			source => "puppet:///modules/users/$name/.bashrc"
+		}
+		-> file{"${user_home_dir}/.gitconfig":
+			ensure => file,
+			owner => $name,
+			group => $name,
+			mode => '0640',
+			source => "puppet:///modules/users/$name/.gitconfig"
+		}
+		-> file{"${user_home_dir}/.forward":
+			ensure => present,
+			owner  => warren,
+			group  => warren,
+			mode   => '0755',
+			content => $email
+		}
+
+		exec {"$name vscode config":
+			command => "/bin/mkdir -p ${user_home_dir}/.config/Code/User && /bin/chown -R ${name}:${name} ${user_home_dir}/.config/Code/User",
+			creates => "${user_home_dir}/.config/Code/User",
+			require => User[$name]
+		}
+		-> file{"${user_home_dir}/.config/Code/User/settings.json":
+			ensure => present,
+			owner  => warren,
+			group  => warren,
+			mode   => '0664',
+			content => "puppet:///modules/users/$name/.config/Code/User/settings.json"
+		}
+	}
 }

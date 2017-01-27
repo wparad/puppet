dbb6c58c7a8780999cd55c59852edbbfbc820014
Warren Parad <wparad@gmail.com>
2017-01-27 14:29:19 +0100

    Fix sublime and remove meld from git.

--- modules/sublime/manifests/init.pp
+++ modules/sublime/manifests/init.pp
@@ -4,6 +4,9 @@ class sublime(){
 		mode => '0644',
 		source => 'puppet:///modules/sublime/webupd8team-ubuntu-sublime-text-3-xenial.list',
 	}
+	~> exec {'Add PPA key':
+		command => 'sudo add-apt-repository ppa:webupd8team/sublime-text-3'
+	}
         -> Exec['update apt']
 	-> package{'sublime-text-installer':}
 }

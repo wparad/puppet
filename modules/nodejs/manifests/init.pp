a2aafee03940ab63dccaa2c6c78b357fe5e3be4a
Warren Parad <wparad@gmail.com>
2018-09-18 16:00:08 +0200

    Add fkill-cli

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -16,7 +16,8 @@ class nodejs($user = 'warren')
         path => $::path,
         creates => "/home/${user}/.nvm/nvm.sh"
     }
-    -> exec { "/bin/bash -c 'source /home/${user}/.nvm/nvm.sh && nvm install 8.9.3 && /home/${user}/.yarn/bin/yarn global add mirri diff-so-fancy'":
+    # https://github.com/sindresorhus/fkill-cli
+    -> exec { "/bin/bash -c 'source /home/${user}/.nvm/nvm.sh && nvm install 8.9.3 && /home/${user}/.yarn/bin/yarn global add mirri diff-so-fancy fkill-cli'":
         path => $::path,
         user => $user
     }

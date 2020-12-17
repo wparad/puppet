60fec8c07314bb5af93a16b9668532fc40ab276e
Warren Parad <wparad@gmail.com>
2020-12-17 17:22:21 +0100

    add tweaks for ddr.

--- rpi.sh
+++ rpi.sh
@@ -21,14 +21,40 @@ EOF
 # Build step mania
 cd raspbian-stepmania-arcade
 make
+make arcade-setup
 
 sudo apt update
+
 # Bluetooth enabled https://howchoo.com/pi/bluetooth-raspberry-pi
-sudo apt install bluetooth pi-bluetooth bluez blueman
-# If the sound doesn't work revalidate the asoundrc file
-# sudo rm /etc/modprobe.d/usb-audio-by-default.conf
-# rm ~/.asoundrc
+sudo apt install bluetooth pi-bluetooth bluez blueman joystick jstest-gtk
 
 ## Custom dance pads
 # https://www.popsci.com/story/diy/build-arcade-dance-game/
 # sudo apt install arduino
+
+# Change size of desktop and stepmania
+=> 1600 x 900
+
+# AudioSync .120
+
+# Get Noteskins https://www.reddit.com/r/Stepmania/comments/4ngqme/any_good_noteskins_for_51/
+mkdir -p ~/.stepmania-5.1/NoteSkins/dance
+
+#Mount remote share
+//palladium.warrenparad.net/ddr/ /home/pi/.stepmania-5.1/Songs cifs credentials=/home/pi/credentialsrw,user,relatime,cache=strict,soft,echo_interval=60,actimeo=1,_netdev,x-systemd.automount,x-systemd.device-timeout=30,noperm,nofail 0 0
+
+
+
+## Debugging
+`udevadm info /dev/input/event0 | awk -F'=' '/ID_SERIAL=(.*)/{print $2}'`
+# => 0079_USB_Gamepad
+`ls -hal /dev/input/by-id`
+# =>
+# lrwxrwxrwx 1 root root   9 Dec 17 14:39 usb-0079_USB_Gamepad-event-joystick -> ../event0
+# lrwxrwxrwx 1 root root   6 Dec 17 14:39 usb-0079_USB_Gamepad-joystick -> ../js0
+
+## Prevent Sleeping https://stackoverflow.com/questions/30985964/how-to-disable-sleeping-on-raspberry-pi
+echo "xserver-command=X -s 0 -p 0 -dpms" >> /etc/lightdm/lightdm.conf
+
+# Fullscreen exit: https://github.com/SpottyMatt/raspbian-stepmania-arcade/blob/master/arcade-setup/user-settings/Save/Default-Preferences.ini
+alt + enter
